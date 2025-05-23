351434330da09170551ecfeda96322ba6bc750e3
VitaliiPokotilo <vitaliipokotilo15@gmail.com>
2025-05-23 11:12:40 +0300

    Add views

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Image
+
+from .models import Category
+
+def gallery_view(request):
+    categories = Category.objects.all()
+    return render(request, 'gallery.html', {'categories': categories})
+
+
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+    return render(request, 'image_detail.html', {'image': image})
